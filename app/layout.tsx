ddcc33e0348a1735b72b229bf7ca007641e43aeb
Tran Ngoc Thang <tranngocthang.bkdn@gmail.com>
2023-08-14 22:09:00 +0700

    Add metadata and icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,14 +5,22 @@ import { Lato } from "next/font/google";
 const inter = Lato({ subsets: ["latin"], weight: ["400"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://dropbyte.org"),
   title: "Dropbyte - Simple file sharing app",
   description:
     "Effortless File Sharing: Upload, Store and Share with Ease | Dropbyte",
   authors: { name: "Liquiddev99", url: "https://github.com/liquiddev99" },
   openGraph: {
     title: "Dropbyte - Simple file sharing app",
+    siteName: "Dropbyte",
+    url: "https://dropbyte.org",
     description:
       "Effortless File Sharing: Upload, Store and Share with Ease | Dropbyte",
+    images: "/logo.png",
+    type: "website",
+  },
+  icons: {
+    apple: "/apple-icon.png",
   },
 };
 
